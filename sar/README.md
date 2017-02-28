bfbaf87c45850c9b644a3a46580910f40faf10ae
David Sutherin <sutherin.3@wright.edu>
2017-02-28 14:47:32 -0500

    Adds code to print potentiometer readings to console

--- sar/README.md
+++ sar/README.md
@@ -1,5 +1,9 @@
 # Simple Analog Reader (sar)
 Following [this tutorial](https://www.arduino.cc/en/Tutorial/AnalogInput) to get the hang of doing analog input reads with an Arduino
 
+### Got it working! Woohoo!
+Now, on to the next project...
+
 ### Resources
-[Analog input reading](https://www.arduino.cc/en/Tutorial/AnalogInput)
+[Analog input tutorial](https://www.arduino.cc/en/Tutorial/AnalogInput)
+[Potentiometer tutorial](https://www.arduino.cc/en/Tutorial/Potentiometer)
---
+++ sar/potentiometer/potentiometer.ino
@@ -0,0 +1,33 @@
+int pot = 0;
+int led = 13;
+int val = 0;
+unsigned long prev = 0, now;
+uint16_t uVal;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(pot);
+  now = millis();
+  //Serial.print(now);
+  //Serial.print(" ");
+  //Serial.println(val);
+  if (now > (prev+500)) {
+    prev = now;
+    uVal = (uint16_t) val;
+    Serial.print("Time: ");
+    Serial.println(now);
+    Serial.print("Value: ");
+    Serial.println(val);
+    Serial.print("uVal: ");
+    Serial.println(uVal);
+    Serial.println();
+  }
+  if (val > 500) {  digitalWrite(led,HIGH); }
+  else {  digitalWrite(led,LOW);  }
+}
